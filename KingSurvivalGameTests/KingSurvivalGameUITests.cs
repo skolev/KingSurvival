77b1cd3c84fe5de377b393acd7c83a885163bd73
skolev <svetliokolev@gmail.com>
2013-05-15 13:05:19 +0300

    Added Unit test project to excisting solution.
    Added Unit test for the game grid.

---
+++ KingSurvivalGameTests/KingSurvivalGameUITests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using KingSurvivalGame.Common;
+using KingSurvivalGame.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KingSurvivalGameTests
+{
+    [TestClass]
+    public class KingSurvivalGameUITests
+    {
+        [TestMethod]
+        public void TestGameGrid()
+        {
+            Game testGame = new Game();
+            string resultField = testGame.GetGridAsString();
+            string expected = string.Format(
+                "{0}" +
+                "UL  0 1 2 3 4 5 6 7  UR{0}" +
+                "   _________________   {0}" +
+                "0 | A   B   C   D   | 0{0}" +
+                "1 |                 | 1{0}" +
+                "2 |                 | 2{0}" +
+                "3 |                 | 3{0}" +
+                "4 |                 | 4{0}" +
+                "5 |                 | 5{0}" +
+                "6 |                 | 6{0}" +
+                "7 |       K         | 7{0}" +
+                "  |_________________|  {0}" +
+                "DL  0 1 2 3 4 5 6 7  DR{0}{0}",
+                Environment.NewLine
+                );
+            Assert.AreEqual<string>(expected, resultField);
+        }
+
+        // TODO example game session test
+    }
+}
