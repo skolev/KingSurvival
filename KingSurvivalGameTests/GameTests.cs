33f1dc486950d361d1b6ec6f56498c7450d6efb5
skolev <svetliokolev@gmail.com>
2013-05-16 23:50:42 +0300

    Added Game.cs unit tests

---
+++ KingSurvivalGameTests/GameTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Reflection;
+using KingSurvivalGame.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KingSurvivalGame.Tests
+{
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void TestGameGrid()
+        {
+            Game testGame = new Game();
+            string resultField = testGame.GetGridAsString();
+            string expected = string.Format(
+                "{0}" +
+                "UL 01234567 UR{0}" +
+                "   ________   {0}" +
+                "0 |A+B+C+D+| 0{0}" +
+                "1 |+-+-+-+-| 1{0}" +
+                "2 |-+-+-+-+| 2{0}" +
+                "3 |+-+-+-+-| 3{0}" +
+                "4 |-+-+-+-+| 4{0}" +
+                "5 |+-+-+-+-| 5{0}" +
+                "6 |-+-+-+-+| 6{0}" +
+                "7 |+-+K+-+-| 7{0}" +
+                "  |________|  {0}" +
+                "DL 01234567 DR{0}{0}",
+                Environment.NewLine
+                );
+            Assert.AreEqual<string>(expected, resultField);
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void TestKingExitedsuccessfully()
+        {
+            Game game = new Game();
+            //game.KingYPosition = 2;
+            bool result = game.CheckIfKingExited();
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void TestGetPawnDestinationDownRight()
+        {
+            BindingFlags bindingnFlags = BindingFlags.Instance | BindingFlags.NonPublic;
+            Game game = new Game();
+            int[] values = new int[] {2, 3};
+            object[] arguments = new object[] {values, 'R'};
+            MethodInfo testedMethod = typeof(Game).GetMethod("GetPawnDestination", bindingnFlags);
+            int[] result = (int[])testedMethod.Invoke(game, arguments);
+            int[] expected = new int[] { 3, 4 };
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestGetPawnDestinationDownLeft()
+        {
+            BindingFlags bindingnFlags = BindingFlags.Instance | BindingFlags.NonPublic;
+            Game game = new Game();
+            int[] values = new int[] { 2, 5 };
+            object[] arguments = new object[] { values, 'L' };
+            MethodInfo testedMethod = typeof(Game).GetMethod("GetPawnDestination", bindingnFlags);
+            int[] result = (int[])testedMethod.Invoke(game, arguments);
+            int[] expected = new int[] { 3, 4 };
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestPlayPawnAMoveLeft()
+        {
+            Game game = new Game();
+            bool result = game.PlayPawnMove('A', 'L');
+            bool expected = false;
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestPlayPawnAMoveRight()
+        {
+            Game game = new Game();
+            bool result = game.PlayPawnMove('A', 'R');
+            bool expected = true;
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestPlayPawnDMoveLeft()
+        {
+            Game game = new Game();
+            bool result = game.PlayPawnMove('D', 'L');
+            bool expected = true;
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestPlayPawnDMoveRight()
+        {
+            Game game = new Game();
+            bool result = game.PlayPawnMove('D', 'R');
+            bool expected = true;
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestPlayPawnInvalidPawn()
+        {
+            Game game = new Game();
+            game.PlayPawnMove('E', 'R');
+        }
+    }
+}
